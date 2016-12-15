a8495727a9517d22ded55c5e18a4fffa39cc1517
shashikanthjogi <shashikanth.jogi@gmail.com>
2016-12-15 08:04:06 +0530

    Added location to the column in the grid

--- MyAngApp/SPA/Controller/GridController.js
+++ MyAngApp/SPA/Controller/GridController.js
@@ -13,18 +13,28 @@
         $scope.data.lowstockdata.data = [];
         for (i = 0; i < $scope.data.lowstockdata.itemsperpage; i++)
         {
+            var currLoation = $scope.selectedLocation.Location ; 
             var rndnum = ($scope.data.lowstockdata.currentPage * 10) + i;
             $scope.data.lowstockdata.data.push({
                 SKU: "SKU" + rndnum,
                 ProductTitle: "ProductTitle" + rndnum,
                 OnOrder: rndnum * 2,
                 Due: rndnum - 1,
-                StockLevel: rndnum
+                StockLevel: rndnum,
+                Location: currLoation
             });
         }
     }
 
     GetData();
+    $scope.pageChanged = function () {
+        GetData();
+    }
+
+    $scope.$watch('selectedLocation', function () {
+        $scope.data.lowstockdata.currentPage = 1;
+        GetData();
+    });
 }
 
 GridController.$inject = ['$scope'];
