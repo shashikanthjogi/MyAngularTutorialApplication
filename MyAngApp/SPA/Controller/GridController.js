66823126aead0891e6fe80128ad3e803bef5370c
shashikanthjogi <shashikanth.jogi@gmail.com>
2016-12-25 09:36:35 +0530

    mades changes for modal and chart is not working

--- MyAngApp/SPA/Controller/GridController.js
+++ MyAngApp/SPA/Controller/GridController.js
@@ -1,4 +1,4 @@
-﻿var GridController = function ($scope) {
+﻿var GridController = function ($scope, $uibModal) {
     $scope.data = {
         lowstockdata: {
             totalitems: 726,
@@ -8,12 +8,10 @@
         }
     };
 
-    function GetData()
-    {
+    function GetData() {
         $scope.data.lowstockdata.data = [];
-        for (i = 0; i < $scope.data.lowstockdata.itemsperpage; i++)
-        {
-            var currLoation = $scope.selectedLocation.Location ; 
+        for (i = 0; i < $scope.data.lowstockdata.itemsperpage; i++) {
+            var currLoation = $scope.selectedLocation.Location;
             var rndnum = ($scope.data.lowstockdata.currentPage * 10) + i;
             $scope.data.lowstockdata.data.push({
                 SKU: "SKU" + rndnum,
@@ -35,6 +33,24 @@
         $scope.data.lowstockdata.currentPage = 1;
         GetData();
     });
+
+    $scope.openProduct = function (product) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'SPA/Views/ViewProductWindow.html',
+            controller: 'ViewProductController',
+            size: "",
+            resolve: { data: product }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.data.lowstockdata.selectedItem = selectedItem;
+        }, function () {
+            alert('error');
+            $log.info('Modal dismissed at' + new Date());
+        });
+    }
 }
 
-GridController.$inject = ['$scope'];
+
+GridController.$inject = ['$scope', '$uibModal'];
